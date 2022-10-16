39e8e3df4111c61b938ff720f3918dcd88120cd4
franckapik <fanch44@hotmail.com>
2022-10-16 23:47:39 +0200

    nextjs react bootstrap

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import "../public/style/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//auto-generated ids are consistent between the server and client.
+import SSRProvider from "react-bootstrap/SSRProvider";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  );
+}
