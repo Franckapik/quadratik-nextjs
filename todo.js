d5795a7ed93ebab0d3f936e92c3cb09bbf279c76
Franckapik <fanch44@hotmail.com>
2022-12-06 21:20:07 +0100

    update to do on product page

--- todo.js
+++ todo.js
@@ -2,13 +2,26 @@
 
 refresh plusieurs fois explorer
 
- {/* 
-    posts &&
-    posts.map((a, i) => (
-     
-     <Form.Group className="mb-3">
-        <Form.Label>{a.label}</Form.Label>
-        {<Values valueId={a.id}></Values>}{" "}
-      </Form.Group>
+=> product/performances
+  * chargement dynamique des fichiers csv
+  * chargement dynamique des fichiers plots
 
-    )) */}
+=> product/3D
+  * loader de chargement
+  * auto zoom sur le modele 
+
+=> product/url
+  * choisir bonnes variables en queryState
+  * chargement en fonction de l'url => defaultValue à prendre en compte dès le debut => produit initial
+
+=> product/ajout de variant
+  * post du produit à régler 
+
+=> product/css
+  *integration des différentes options au dessus du canvas
+  * optimisation de la place des selects menu
+
+=> products/shop 
+  * affichage des différents produits "phares" en boutique
+
+=> products/
