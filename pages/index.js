45dad17afe7a016aad39036f28142bd85ad0e153
Franckapik <fanch44@hotmail.com>
2023-01-26 10:57:30 +0100

    using parallax

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,40 @@
 import { Container } from "react-bootstrap";
 import { useSpring, animated, useScroll } from "@react-spring/web";
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+
 
 const Main = () => {
-  const { scrollYProgress } = useScroll();
-
-  const [springs, api] = useSpring(() => ({
-    from : {transform : 'rotate(0deg)'},
-    to : {transform : 'rotate(45deg)'}
-  }));
-
-  const handleClick = () => {
-    api.start({
-      from: {
-        opacity: 0,
-      },
-      to: {
-        opacity: 100,
-      },
-    });
-  };
-
-  return (
-    <Container style={{ height: "600vh" }} fluid>
-      <animated.div
-        style={{
-          position: "fixed",
-          opacity: 1,
-          height: scrollYProgress.to((val) => val * 100 + "vh"),
-          background: "#ff6d6d",
-          borderRadius: 8,
-        }}
-      >
-        {" "}
-        Coucou
-      </animated.div>
-      <div style={{ background: "red", height: "100vh" }}>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
-          <animated.div style={{ width: "33vw", height: "33vw", background: "blue", position:"sticky", top :"20px",...springs }}></animated.div>
-        </div>
+    const alignCenter = { display: 'flex', alignItems: 'center' }
+    return (
+      <div>
+        <div/>
+  
+        <Parallax pages={5}>
+          <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
+            <p >Scroll down</p>
+          </ParallaxLayer>
+  
+          <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+            <div >
+              <p>I'm a sticky layer</p>
+            </div>
+          </ParallaxLayer>
+  
+          <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+            <div >
+              <p>I'm not</p>
+            </div>
+          </ParallaxLayer>
+  
+          <ParallaxLayer offset={2.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+            <div >
+              <p>Neither am I</p>
+            </div>
+          </ParallaxLayer>
+        </Parallax>
       </div>
-      <div style={{ background: "blue", height: "100vh" }}></div>
-      <div style={{ background: "green", height: "100vh" }}></div>
-      <div style={{ background: "pink", height: "100vh" }}></div>
-      <div style={{ background: "yellow", height: "100vh" }}></div>
-      <div style={{ background: "purple", height: "100vh" }}></div>
-    </Container>
-  );
+    )
+  
 };
 
 export default Main;
