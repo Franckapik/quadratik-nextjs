5c4e02ae38bb412f5dd39d72cdc5dee478e6455b
Franckapik <fanch44@hotmail.com>
2023-01-26 09:57:29 +0100

    rotateY with scroll

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,50 @@
-import { Container } from "react-bootstrap"
-import { useSpring, animated, useScroll } from '@react-spring/web'
+import { Container } from "react-bootstrap";
+import { useSpring, animated, useScroll } from "@react-spring/web";
 
 const Main = () => {
+  const { scrollYProgress } = useScroll();
 
-    const { scrollYProgress } = useScroll()
+  const [springs, api] = useSpring(() => ({
+    from: { opacity: 0 },
+  }));
 
-    const [springs, api] = useSpring(() => ({
-        from: { opacity: 0 },
-      }))
+  const handleClick = () => {
+    api.start({
+      from: {
+        opacity: 0,
+      },
+      to: {
+        opacity: 100,
+      },
+    });
+  };
 
-      const handleClick = () => {
-        api.start({
-          from: {
-            opacity: 0,
-          },
-          to: {
-            opacity: 100,
-          },
-        })
-      }
-    
-
-
-    return (
-       
-
-        <Container style={{height : "500vh"}} fluid>
-            <animated.div
+  return (
+    <Container style={{ height: "600vh" }} fluid>
+      <animated.div
         style={{
-            position : "fixed",
-        opacity: 1,
-          height: scrollYProgress.to(val => val * 100 + "vh"),
-          background: '#ff6d6d',
+          position: "fixed",
+          opacity: 1,
+          height: scrollYProgress.to((val) => val * 100 + "vh"),
+          background: "#ff6d6d",
           borderRadius: 8,
         }}
-      > Coucou</animated.div>
-        </Container>
-    )
-}
+      >
+        {" "}
+        Coucou
+      </animated.div>
+      <div style={{ background: "red", height: "100vh" }}>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+          <animated.div style={{ width: "33vw", height: "33vw", background: "blue", transform: scrollYProgress.to(val => `rotateY(${val*10}rad)`), position:"sticky", top :"20px" }}></animated.div>
+        </div>
+      </div>
+      <div style={{ background: "blue", height: "100vh" }}></div>
+      <div style={{ background: "green", height: "100vh" }}></div>
+      <div style={{ background: "pink", height: "100vh" }}></div>
+      <div style={{ background: "yellow", height: "100vh" }}></div>
+      <div style={{ background: "purple", height: "100vh" }}></div>
+    </Container>
+  );
+};
 
-export default Main
+export default Main;
