9c10092d56683d0be86c341e188576417fced77f
Franckapik <fanch44@hotmail.com>
2023-04-19 12:34:12 +0200

    trying performances fmin to cwidth

--- components/product/Modele3D.js
+++ components/product/Modele3D.js
@@ -5,8 +5,10 @@ import Shop3D from "../Shop3D";
 export const Modele3D = () => {
   const price = useProductStore.getState().price
   const nomenclature = useProductStore.getState().nomenclature
-  /*   const {fmin, fmax, cwidth} = useProductStore((state) => ({fmin : state.fmin, fmax : state.fmax, cwidth : state.cwidth})) */
-console.log("coucou");
+  const fmin = useProductStore.getState().fmin
+  const fmax = useProductStore.getState().fmax
+  const cwidth = useProductStore.getState().cwidth
+  
   return (
     <Row className="product_modele_main h-100">
       <Col md={5} className="d-flex flex-column justify-content-evenly ps-5">
@@ -19,10 +21,10 @@ console.log("coucou");
         <Row></Row>
         <Row className="align-items-center">
           <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">S</div>
-          {"fmin"} Hz - {"fmax"} Hz
+          {fmin} Hz - {fmax} Hz
         </Row>
         <Row className="align-items-center">
-          <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">E</div> 40 mm
+          <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">E</div> {(cwidth*10).toFixed(0)} mm
         </Row>
         <Row className="align-items-center">
           <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">D</div> 500x 100 x 5 mm
