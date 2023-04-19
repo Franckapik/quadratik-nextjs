387c41c379221fc7d66d6e2e4126dabbb1892840
Franckapik <fanch44@hotmail.com>
2023-04-19 11:29:42 +0200

    hook values3D in the hook nomenclature

--- hooks/useNomenclature.js
+++ hooks/useNomenclature.js
@@ -1,32 +1,14 @@
 import { useEffect, useState } from "react";
 import { useProductStore } from "./store";
+import { useValues3D } from "./useValues3D";
 
 export const useNomenclature = (valuesSelected, defaultProduct, attributes) => {
   const [nomenclature, setNomenclature] = useState(false);
   useProductStore.setState({ nomenclature: nomenclature });
+  const dimensions = useValues3D(valuesSelected, attributes);
 
-
-  useEffect(() => {
-    const listOfValues = Object.entries(attributes).reduce((acc, [i, a] = cur) => {
-      for (let key in a.values) {
-        acc.push(a.values[key]);
-      }
-      return acc;
-    }, []);
-  
-    const listOfvRef = Object.entries(valuesSelected).reduce((acc, [i, a] = cur) => {
-      const pickValue = listOfValues.filter((value) => value.v_id === a)[0];
-      if (pickValue !== undefined) {
-        return {
-          ...acc,
-          [i]: pickValue.v_3d,
-        };
-      } else {
-        return acc;
-      }
-    }, 0);
-   
-    const {C, D, E, I, L, M, N, P, W, H, V} = listOfvRef;
+  useEffect(() => {  
+    const {C, D, E, I, L, M, N, P, W, H, V} = dimensions;
 
     const n = {
       structurel: D + "N" + N + "W" + W + "L" + L + "P" + P + "E" + E + M,
@@ -35,7 +17,7 @@ export const useNomenclature = (valuesSelected, defaultProduct, attributes) => {
     };
 
     setNomenclature(n);
-  }, [valuesSelected]);
+  }, [dimensions]);
 
   return nomenclature;
 };
