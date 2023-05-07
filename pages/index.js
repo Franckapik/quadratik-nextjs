fd094ecff99ea18feeaffba7d379f5b78c9f75e7
Franckapik <fanch44@hotmail.com>
2023-05-07 22:29:37 +0200

    delete old files

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import { useEffect, useRef } from "react";
 import { Layout } from "../components/Layout";
 import { S0_Landing } from "../components/home/S0_Landing";
 import { S1_Product } from "../components/home/S1_Product";
-import { S2_Canvas } from '../components/home/S2_Canvas';
+import { S2_Canvas } from "../components/home/S2_Canvas";
 import { S2_Customers } from "../components/home/S2_Customers";
 import { S3_DIY } from "../components/home/S3_DIY";
 import { S4_Business } from "../components/home/S4_Business";
 import { S5_Contact } from "../components/home/S5_Contact";
 import { VerticalSideIndex } from "../components/home/VerticalSideIndex";
-import { useBearStore } from '../hooks/store';
+import { useBearStore } from "../hooks/store";
 import { Burger } from "../components/Burger";
 
 const Home = () => {
@@ -17,7 +17,7 @@ const Home = () => {
 
   useEffect(() => {
     const getScroll = (e) => {
-      useBearStore.setState({scroll : e.target.scrollTop} )
+      useBearStore.setState({ scroll: e.target.scrollTop });
     };
     const container = parallax.current.container.current;
     container.addEventListener("scroll", getScroll);
@@ -27,38 +27,38 @@ const Home = () => {
     };
   });
 
-  
   useEffect(() => {
-    useBearStore.setState({width :parallax.current.container.current.offsetWidth} )
-    useBearStore.setState({height : parallax.current.space } )
+    useBearStore.setState({ width: parallax.current.container.current.offsetWidth });
+    useBearStore.setState({ height: parallax.current.space });
   }, []);
 
   return (
-    <><Burger></Burger>  <VerticalSideIndex ></VerticalSideIndex> 
+    <>
+      <Burger></Burger> <VerticalSideIndex></VerticalSideIndex>
       <Parallax pages={8} ref={parallax}>
-        <ParallaxLayer offset={0} >
+        <ParallaxLayer offset={0}>
           <S0_Landing />
         </ParallaxLayer>
-        <ParallaxLayer offset={1}  sticky={{ start: 1, end: 2 }}>
+        <ParallaxLayer offset={1} sticky={{ start: 1, end: 2 }}>
           <S1_Product />
         </ParallaxLayer>
-        <ParallaxLayer offset={3} >
-        <S2_Customers />
+        <ParallaxLayer offset={3}>
+          <S2_Customers />
         </ParallaxLayer>
-        <ParallaxLayer offset={4} >
-        <S2_Canvas  />
+        <ParallaxLayer offset={4}>
+          <S2_Canvas />
         </ParallaxLayer>
-        <ParallaxLayer offset={5} >
+        <ParallaxLayer offset={5}>
           <S3_DIY />
         </ParallaxLayer>
-        <ParallaxLayer offset={6} >
+        <ParallaxLayer offset={6}>
           <S4_Business />
         </ParallaxLayer>
-        <ParallaxLayer offset={7} >
+        <ParallaxLayer offset={7}>
           <S5_Contact />
         </ParallaxLayer>
-      </Parallax></>
-    
+      </Parallax>
+    </>
   );
 };
 
