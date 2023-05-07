ef44d68aa091a9d9b8ad28bd42587ca0dd578e4b
Franckapik <fanch44@hotmail.com>
2023-05-07 21:53:56 +0200

    icon in factorized performance row in product

--- components/product/ProductHud.js
+++ components/product/ProductHud.js
@@ -1,41 +1,51 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 import { useProductStore } from "../../hooks/store";
 import ProductCanvas from "./ProductCanvas";
 
-export const ProductHud = () => {
+const PerformanceRow = ({value, text, icon}) => (
+  <OverlayTrigger
+    key={"left"}
+    placement={"left"}
+    overlay={
+      <Tooltip id={`tooltip-freq`}>
+        <span className="p-4">{text}</span>
+      </Tooltip>
+    }
+  >
+    <Row className="align-items-center">
+      <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">
+        <i className={icon} />
+      </div>
+     {value}
+    </Row>
+  </OverlayTrigger>
+);
 
+export const ProductHud = () => {
   //just on page render
-  const price = useProductStore.getState().price
-  const nomenclature = useProductStore.getState().nomenclature
+  const price = useProductStore.getState().price;
+  const nomenclature = useProductStore.getState().nomenclature;
 
   //need to re-render the page
-  const fmin = useProductStore(state => state.fmin);
-  const fmax = useProductStore(state => state.fmax);
-  const cwidth = useProductStore(state => state.cwidth);
-  const sizes = useProductStore(state => state.sizes);
-  
+  const fmin = useProductStore((state) => state.fmin);
+  const fmax = useProductStore((state) => state.fmax);
+  const cwidth = useProductStore((state) => state.cwidth);
+  const sizes = useProductStore((state) => state.sizes);
+
   return (
     <Row className="">
-      <Col md={4} className="d-flex flex-column justify-content-evenly ps-5">
+      <Col md={5} className="d-flex flex-column justify-content-evenly ps-5">
         <Row>
           <p className="text-uppercase ft05">
-            {nomenclature?.simple}  <p className=" ft1 mt-3 text_green bg_darker">{price + " €"}</p>
+            {nomenclature?.simple} <p className=" ft1 mt-3 text_green bg_darker">{price + " €"}</p>
           </p>
-         
-      </Row>
-        <Row className="align-items-center">
-          <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">S</div>
-          {fmin} Hz - {fmax} Hz
         </Row>
-        <Row className="align-items-center">
-          <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">C</div> {(cwidth*10).toFixed(0)} mm
-        </Row>
-        <Row className="align-items-center">
-          <div className="product_list_square border_creme d-flex justify-content-center align-items-center me-4">D</div> {sizes.longueur} x {sizes.largeur} x {sizes.profondeur} cm
-        </Row>
+        <PerformanceRow value= {`${fmin} Hz - ${fmax} Hz`} text="Spectre de fréquences traité" icon="fad fa-bolt ft2" />
+        <PerformanceRow value= {`${(cwidth * 10).toFixed(0)} mm`} text="Taille d'une cellule" icon="fad fa-sort-size-down ft2"/>
+        <PerformanceRow value= {`${sizes.longueur} x ${sizes.largeur} x ${sizes.profondeur} cm`} text="Spectre de fréquences traité" icon="fad fa-box-open ft2"/>
         <Row></Row>
       </Col>
-      <Col md={8} className="product_canvas_container">
+      <Col md={7} className="product_canvas_container">
         <ProductCanvas></ProductCanvas>
       </Col>
     </Row>
