92f18bf8a8f8cac8ba5207ff9fedd0eb39e90c99
Franckapik <fanch44@hotmail.com>
2023-05-09 20:43:22 +0200

    trying offcanvas

--- components/Burger.js
+++ components/Burger.js
@@ -1,9 +1,14 @@
 import { animated, useInView } from "@react-spring/web";
 import { Turn as Hamburger } from "hamburger-react";
 import { useState } from "react";
-import { Col } from "react-bootstrap";
+import { Col, Offcanvas } from "react-bootstrap";
 
 export const Burger = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
+
   const [isOpen, setOpen] = useState(false);
   const [ref, springs] = useInView(
     () => ({
@@ -18,8 +23,16 @@ export const Burger = () => {
   );
 
   return (
-    <animated.div ref={ref} style={springs} className="burger">
-        <Hamburger toggled={isOpen} toggle={setOpen} color="#FFFFFF" />
-    </animated.div>
+    <>
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.</Offcanvas.Body>
+      </Offcanvas>
+      <animated.div ref={ref} style={springs} className="burger">
+        <Hamburger onClick={toggleShow} toggled={isOpen} toggle={setOpen} color="#FFFFFF" />
+      </animated.div>
+    </>
   );
 };
