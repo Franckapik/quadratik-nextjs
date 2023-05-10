2d9558a99d0fc1d843be87898f01b40f9b93cc78
Franckapik <fanch44@hotmail.com>
2023-05-10 09:13:08 +0200

    pure css animation swipe on cards

--- components/home/S4_Business.js
+++ components/home/S4_Business.js
@@ -4,13 +4,13 @@ import Marquee from "react-fast-marquee";
 import { useDrag } from "react-use-gesture";
 import { useSprings, animated, to as interpolate, useInView, useSpring } from "@react-spring/web";
 import Moment from "react-moment";
-import 'moment/locale/fr';
+import "moment/locale/fr";
 
 const cards = ["./carte_savoirfaire.jpg", "./carte_ecoute.jpg", "./carte_ecoresponsable.jpg"];
 
 // Initials values of cards props.  These two are just helpers, they conserve spring data, values that are later being interpolated into css
 const to = (i) => ({
-  x: 0, 
+  x: 0,
   y: i * -4,
   scale: 1,
   rot: -10 + Math.random() * 20,
@@ -85,8 +85,8 @@ const Deck = ({ setVisibleCard }) => {
 
 export const S4_Business = () => {
   const [visibleCard, setVisibleCard] = useState(3);
-  const [ref, inView] = useInView({once : true});
-
+  const [ref, inView] = useInView({ once: true });
+  /* 
   const [cursorRef, springs] = useInView(
     () => ({
       from: { left: 0 },
@@ -94,7 +94,7 @@ export const S4_Business = () => {
       reverse: true,
     })
 
-  )
+  ) */
 
   const switchList = (visibleCard) => {
     switch (visibleCard) {
@@ -114,32 +114,31 @@ export const S4_Business = () => {
         );
       case 3:
         return (
-      <Row className=" flex-md-column justify-content-center align-items-center ">
-
+          <Row className=" flex-md-column justify-content-center align-items-center ">
             <Col className="align-items-center d-flex flex-column flex-md-row p-md-4 ps-0 bg">
               <img src="./icones/bois.svg" className="s4_list_icon me-md-5 m-2" /> Bois PEFC/FSC
-           </Col>
-           <Col className="d-flex align-items-center d-flex flex-column flex-md-row p-md-4 ps-0">
+            </Col>
+            <Col className="d-flex align-items-center d-flex flex-column flex-md-row p-md-4 ps-0">
               <img src="./icones/1380.svg" className="s4_list_icon me-md-5 m-2" /> 1380km maximum
-           </Col>
-           <Col className="align-items-center d-flex flex-column flex-md-row p-md-4 ps-0">
+            </Col>
+            <Col className="align-items-center d-flex flex-column flex-md-row p-md-4 ps-0">
               <img src="./icones/plastique.svg" className="s4_list_icon me-md-5 m-2" /> Zero plastique
-           </Col>
-         </Row>
+            </Col>
+          </Row>
         );
       case 2:
         return (
-      <Row className=" flex-md-column justify-content-center align-items-center">
+          <Row className=" flex-md-column justify-content-center align-items-center">
             <Col className="align-items-center d-flex flex-column flex-md-row p-md-4 ps-0">
               <img src="./icones/hache.svg" className="s4_list_icon me-md-5 m-2" /> Travail manuel
-           </Col>
+            </Col>
             <Col className="align-items-center d-flex flex-column flex-md-row p-md-4 ps-0">
               <img src="./icones/controle.svg" className="s4_list_icon me-md-5 m-2" /> Contrôle intégral
-           </Col>
+            </Col>
             <Col className="align-items-center d-flex flex-column flex-md-row p-md-4 ps-0">
               <img src="./icones/finition.svg" className="s4_list_icon me-md-5 m-2" /> Finitions
-           </Col>
-         </Row>
+            </Col>
+          </Row>
         );
       default:
         return null;
@@ -152,36 +151,25 @@ export const S4_Business = () => {
         return (
           <div className="m-md-5">
             <p className="ft1 pt-4">Ecoresponsabilité</p>
-            L'intégralité des matières premières est issue du territoire Français. Les livraisons sont assurées
-            uniquement en France afin de garantir une empreinte carbone réduite. Le choix du bois contribue à assurer la
-            pérennité et le renouvellement des forêts. La colle et les peintures utilisées sont garanties sans solvant
-            et faibles emissions COV. Les emballages sont emmenés systématiquement en déchetterie. A l’exception de la
-            mélamine, aucun produit issu de la famille des plastiques est proposé en boutique de l’entreprise. Les
-            matériaux d’emballage et de calage sont garantis en carton et papier.
+            L'intégralité des matières premières est issue du territoire Français. Les livraisons sont assurées uniquement en France afin de garantir une empreinte carbone réduite. Le choix du bois contribue à assurer la pérennité et le renouvellement des forêts. La colle et les peintures utilisées
+            sont garanties sans solvant et faibles emissions COV. Les emballages sont emmenés systématiquement en déchetterie. A l’exception de la mélamine, aucun produit issu de la famille des plastiques est proposé en boutique de l’entreprise. Les matériaux d’emballage et de calage sont garantis
+            en carton et papier.
           </div>
         );
       case 1:
         return (
           <div className="m-md-5">
             <p className=" ft1 pt-4">A votre écoute</p>
-            Du début à la fin, vos interrogations et votre projet sont confiés à un seul artisan responsable de la
-            construction des solutions Quadratik. Les différentes expériences reçues depuis plus de 10 ans dans le
-            domaine du son permettent de répondre à vos questions et de faire correspondre au mieux votre situation à
-            vos envies. Le traitement acoustique d'une pièce, dans la plupart des cas déjà existante, demande une
-            reflexion qui est unique à chaque environnement. Une discussion avec vous sera alors une nouvelle aventure
-            avec son lot de défi et d'adaptation.
+            Du début à la fin, vos interrogations et votre projet sont confiés à un seul artisan responsable de la construction des solutions Quadratik. Les différentes expériences reçues depuis plus de 10 ans dans le domaine du son permettent de répondre à vos questions et de faire correspondre au
+            mieux votre situation à vos envies. Le traitement acoustique d'une pièce, dans la plupart des cas déjà existante, demande une reflexion qui est unique à chaque environnement. Une discussion avec vous sera alors une nouvelle aventure avec son lot de défi et d'adaptation.
           </div>
         );
       case 2:
         return (
           <div className="m-md-5">
             <p className=" ft1 pt-4">Un savoir-faire</p>
-            L’essentiel de la transformation, du conditionnement et de son contrôle est réalisé à la main. Les solutions
-            acoustiques Quadratik ne sont pas issues d'un procédé industriel et se dispensent ainsi des erreurs liées au
-            commerce de masse. La qualité esthetique des produits est fondamentale avant l'envoi vers une nouvelle
-            destination. La fabrication artisanale permet également d'adapter les finitions de nos solutions acoustiques
-            selon vos conditions. La marque Quadratik continue chaque jour d'être fière de valoriser son savoir-faire à
-            travers ses produits.
+            L’essentiel de la transformation, du conditionnement et de son contrôle est réalisé à la main. Les solutions acoustiques Quadratik ne sont pas issues d'un procédé industriel et se dispensent ainsi des erreurs liées au commerce de masse. La qualité esthetique des produits est fondamentale
+            avant l'envoi vers une nouvelle destination. La fabrication artisanale permet également d'adapter les finitions de nos solutions acoustiques selon vos conditions. La marque Quadratik continue chaque jour d'être fière de valoriser son savoir-faire à travers ses produits.
           </div>
         );
       default:
@@ -194,44 +182,40 @@ export const S4_Business = () => {
       <Row className="p-0 m-0 mt-2 mb-2">
         <Marquee pauseOnHover gradient={false} speed={70} className="s4_marquee ft1">
           <span className="p-5">NOUVEAUTE - Diffuseur en kit à assembler soi-meme - Ideal pour les petits budgets</span>
-          <img
-            src="./logo/logo_marquee.svg"
-            alt="Miniature du logo de l'entreprise Quadratik"
-          />
+          <img src="./logo/logo_marquee.svg" alt="Miniature du logo de l'entreprise Quadratik" />
         </Marquee>
       </Row>
       <Row className="s4_row_business_values p-2">
         <Col md={8} className="d-flex flex-column justify-content-center p-0">
           <Row>
             <Col md={2} className="d-none d-md-flex"></Col>
-            <Col md={4}  className="s4_values_text text-center text-md-start order-md-2 p-md-2 ">
-                  <Row className="mt-md-0 text-uppercase text-creme">
-                    <p className="ft4 pb-md-3">Depuis  <Moment diff="2018" unit="years" /> ans</p>
-                    <p className="ft05 pb-md-3">Quadratik</p>
-                    <p className="ft5">joue cartes sur table !</p>
-                    <p ref={ref}></p> 
-
-                  </Row>
-                <Row className="s4_list ft5">{switchList(visibleCard)}</Row>
+            <Col md={4} className="s4_values_text text-center text-md-start order-md-2 p-md-2 ">
+              <Row className="mt-md-0 text-uppercase text-creme">
+                <p className="ft4 pb-md-3">
+                  Depuis <Moment diff="2018" unit="years" /> ans
+                </p>
+                <p className="ft05 pb-md-3">Quadratik</p>
+                <p className="ft5">joue cartes sur table !</p>
+                <p ref={ref}></p>
+              </Row>
+              <Row className="s4_list ft5">{switchList(visibleCard)}</Row>
             </Col>
-            <Col md={6}  className="order-md-1">
-              <div className={"s4_cards_container"}>
-
-                <img
-                  src="./card_table.svg"
-                  alt="Table des valeurs de l'entreprise Quadratik.fr"
-                  className="s4_card_table"
-                />
+            <Col md={6} className="order-md-1">
+              <div className="s4_cards_container position-relative">
+                <div className="s4_swipe_icon ft05">
+                  <i class="fa fa-long-arrow-left " />
+                  <i className="fad fa-hand-pointer" /> 
+                  <i class="fa fa-long-arrow-right" />
+                </div>
+                <img src="./card_table.svg" alt="Table des valeurs de l'entreprise Quadratik.fr" className="s4_card_table" />
                 {inView ? <Deck setVisibleCard={setVisibleCard} /> : null}
-                <animated.div style={springs} ref={cursorRef} className="s4_drag_icon ft05"><i className="fad fa-hand-pointer"></i></animated.div>
-
-             </div>
+              </div>
             </Col>
           </Row>
           <Row className="d-md-none justify-content-center w-100 mt-2">
-          <Button variant="primary" className="">
-          <i className="fad fa-id-badge"></i> En savoir plus
-              </Button>
+            <Button variant="primary" className="">
+              <i className="fad fa-id-badge"></i> En savoir plus
+            </Button>
           </Row>
         </Col>
 
