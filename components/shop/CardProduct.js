acb9384ad701c24e6bd90f84448283933768e7db
Franckapik <fanch44@hotmail.com>
2023-06-05 17:23:25 +0200

    usePicture if not undefined

--- components/shop/CardProduct.js
+++ components/shop/CardProduct.js
@@ -7,7 +7,7 @@ import { useComputeProduct } from "../../hooks/useComputeProduct";
 
 export const CardProduct = ({ variantId, defaultProductId, childCatId, childCatLabel }) => {
   const { allAttributes, defaultProduct, category, variantAttributes, isAllSucess, allValues } = useFetchProduct(variantId, defaultProductId, childCatId);
-  const { product, isSuccess: productSuccess, changeAttributes } = useComputeProduct(allAttributes, variantAttributes, allValues, category, defaultProduct, isAllSucess, variantId);
+  const { product, isSuccess: productSuccess } = useComputeProduct(allAttributes, variantAttributes, allValues, category, defaultProduct, isAllSucess, variantId);
   const { facePicture: facePicture, sidePicture: sidePicture, isSuccess: pictureSucceed } = usePicture(product.nomenclature?.simple, true); //true for miniature
 
   return (
@@ -15,7 +15,7 @@ export const CardProduct = ({ variantId, defaultProductId, childCatId, childCatL
       {productSuccess ? (
         <CardWrap>
           <Link href={{ pathname: "/shop/product", query: { display: 0, childCat: childCatId, vid: variantId, dpid: defaultProductId } }}>
-            {facePicture ? <img src={`data:image/png;base64,${facePicture}`} /> : "Image non disponible"}
+            {pictureSucceed ? <img src={`data:image/png;base64,${facePicture}`} /> : "Image non disponible"}
             <span className="shop_product_title ft2 ">{product.nomenclature.simple}</span>
             <span className="shop_product_collection ft6 text-uppercase text-nowrap ">{childCatLabel}</span>
             <span className="shop_product_price ft4  text-nowrap ">{product.prices ? product.prices.price + " €" : "Prix non disponible"} </span>
--- hooks/usePicture.js
+++ hooks/usePicture.js
@@ -2,8 +2,9 @@ import { useQuery } from "react-query";
 import { documentByFilename } from "../components/dolibarrApi/fetch";
 
 export const usePicture = (nom, miniature) => {
-  const { data: facePicture, isSuccess: facePictureSucceed } = useQuery(["facePicture", { name: nom, miniature: miniature }], () => documentByFilename(`Modeles/Face/${miniature ? "Miniature/" : "Large/"}${nom}.png`), { staleTime: Infinity, enabled: !!nom });
-  const { data: sidePicture, isSuccess: sidePictureSucceed } = useQuery(["sidePicture", { name: nom, miniature: miniature }], () => documentByFilename(`Modeles/Side/${miniature ? "Miniature/" : "Large/"}${nom}.png`), { staleTime: Infinity, enabled: !!nom });
+
+  const { data: facePicture, isSuccess: facePictureSucceed } = useQuery(["facePicture", { name: nom, miniature: miniature }], () => documentByFilename(`Modeles/Face/${miniature ? "Miniature/" : "Large/"}${nom}.png`), { staleTime: Infinity, enabled: !!nom && nom !== undefined });
+  const { data: sidePicture, isSuccess: sidePictureSucceed } = useQuery(["sidePicture", { name: nom, miniature: miniature }], () => documentByFilename(`Modeles/Side/${miniature ? "Miniature/" : "Large/"}${nom}.png`), { staleTime: Infinity, enabled: !!nom && nom !== undefined });
 
   return { facePicture: facePicture, sidePicture: sidePicture, isSuccess: facePictureSucceed && sidePictureSucceed };
 };
