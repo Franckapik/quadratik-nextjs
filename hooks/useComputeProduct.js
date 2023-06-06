0df9744ca5fe15e2f5b4b0d6584f662f829cc796
Franckapik <fanch44@hotmail.com>
2023-06-06 12:28:37 +0200

    Working variant and novariant

--- hooks/useComputeProduct.js
+++ hooks/useComputeProduct.js
@@ -10,85 +10,72 @@ export const useComputeProduct = (allAttributes, productAttributes, allValues, c
   const [isSuccess, setSuccess] = useState(false);
   const [attributes, setAttributes] = useState(false);
 
-    const changeAttributes = (value, name) => {
-      setAttributes((previousAttributes) => {
-        let newAttributes = previousAttributes;
-        const i = newAttributes.findIndex((obj) => obj.id == name);
-        if (newAttributes[i] !== undefined) {
-          newAttributes[i].fk_prod_attr_val = value[name];
-        }
-        return newAttributes;
-      });
-
-      setAttributes(false); //magical cmd to refresh component
-    };
-
-    useEffect(() => {
-      if (productAttributes !== undefined) {
-        setAttributes(productAttributes);
+  const changeAttributes = (value, name) => {
+    setAttributes((previousAttributes) => {
+      let newAttributes = previousAttributes;
+      const i = newAttributes.findIndex((obj) => obj.id == name);
+      if (newAttributes[i] !== undefined) {
+        newAttributes[i].fk_prod_attr_val = value[name];
       }
-    }),
-      [productAttributes];
+      return newAttributes;
+    });
 
-    const valuesSelected = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_id");
-    const values3D = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_3d");
-    const valuesLabels = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_label");
-    const valuesFactor = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_factor");
-    const valuesOperator = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_operator");
-    const features = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "id", "v_id");
-    const description = useDescription(category, defaultProduct);
-    const { price: price, basePrice: basePrice } = usePrice(defaultProduct, valuesFactor, valuesOperator);
-    const dimensions = useDimensions(values3D);
-    const nomenclature = useNomenclature(defaultProduct, valuesLabels, dimensions);
+    setAttributes(false); //magical cmd to refresh component
+  };
 
-    useEffect(() => {
-      //calculate
+  useEffect(() => {
+    if (productAttributes !== undefined) {
+      setAttributes(productAttributes);
+    }
+  }),
+    [productAttributes];
 
-      if (isAllSuccess) {
-        setProduct((previousProduct) => ({
-          ...previousProduct,
-          attributes: attributes ? attributes : productAttributes,
-          allAttributes: allAttributes,
-          description: description,
-          dimensions: dimensions,
-          nomenclature: nomenclature,
-          prices: { price: price, basePrice: basePrice },
-          features: features,
-          valuesSelected: valuesSelected,
-          values: allValues,
-          variantId: variantId,
-        }));
-        setSuccess(true);
-      }
-    }, [attributes, isAllSuccess]);
+  const valuesSelected = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_id");
+  const values3D = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_3d");
+  const valuesLabels = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_label");
+  const valuesFactor = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_factor");
+  const valuesOperator = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "ref", "v_operator");
+  const features = useValuesSelected(allAttributes, attributes ? attributes : productAttributes, allValues, "id", "v_id");
+  const description = useDescription(category, defaultProduct || productAttributes);
+  const { price: price, basePrice: basePrice } = usePrice(defaultProduct, valuesFactor, valuesOperator);
+  const dimensions = useDimensions(values3D);
+  const nomenclature = useNomenclature(defaultProduct, valuesLabels, dimensions);
 
-    return { product, isSuccess, changeAttributes };
-  
-/*     console.log("isNOVariant");
+  useEffect(() => {
+    //calculate
 
-    const description = useDescription(category, productAttributes);
+    if (isAllSuccess && isVariant === true) {
+      setProduct((previousProduct) => ({
+        ...previousProduct,
+        attributes: attributes ? attributes : productAttributes,
+        allAttributes: allAttributes,
+        description: description,
+        dimensions: dimensions,
+        nomenclature: nomenclature,
+        prices: { price: price, basePrice: basePrice },
+        features: features,
+        valuesSelected: valuesSelected,
+        values: allValues,
+        variantId: variantId,
+      }));
+      setSuccess(true);
+    } else if (isAllSuccess && isVariant === false) {
+      setProduct((previousProduct) => ({
+        ...previousProduct,
+        attributes: {},
+        allAttributes: {},
+        description: description,
+        dimensions: {},
+        nomenclature: { simple: productAttributes.label, structurel: productAttributes.ref },
+        prices: { price: Math.round(productAttributes.price), basePrice: null },
+        features: {},
+        valuesSelected: {},
+        values: {},
+        variantId: {},
+      }));
+      setSuccess(true);
+    }
+  }, [attributes, isAllSuccess]);
 
-    useEffect(() => {
-      //calculate
-
-      if (isAllSuccess) {
-        setProduct((previousProduct) => ({
-          ...previousProduct,
-          attributes: {},
-          allAttributes: {},
-          description: description,
-          dimensions: {},
-          nomenclature : {simple : productAttributes.label, structurel : productAttributes.ref },
-          prices: { price: Math.round(productAttributes.price), basePrice: null },
-          features: {},
-          valuesSelected: {},
-          values: {},
-          variantId: {},
-        }));
-        setSuccess(true);
-      }
-    }, [isAllSuccess]);
-
-    return { product, isSuccess }; */
-  
+  return { product, isSuccess, changeAttributes };
 };
--- hooks/useFetchProduct.js
+++ hooks/useFetchProduct.js
@@ -4,9 +4,7 @@ import { useValues } from "./useValues";
 
 export const useFetchProduct = (variantId, defaultProductId, childCatId) => {
 
-  console.log(defaultProductId);
-
-  const { data: noVariantAttributes, isSuccess: noVariantSucceed } = useQuery(["noVariant", { id: variantId, onlyId: false }], () => productFetchById(variantId), { staleTime: Infinity,  enabled: defaultProductId != undefined });
+  const { data: noVariantAttributes, isSuccess: noVariantSucceed } = useQuery(["noVariant", { id: variantId, onlyId: false }], () => productFetchById(variantId), { staleTime: Infinity, enabled: defaultProductId != undefined });
   const { data: category, isSuccess: categorySucceed } = useQuery(["category", { childCatId: childCatId }], () => CategoryFetchById(childCatId), { staleTime: Infinity, enabled: !!childCatId });
   const { data: productAttributes, isSuccess: variantsAttributesSucceed } = useQuery(["variants", { defaultProductId: defaultProductId }], () => variantFetchByParentId(defaultProductId), {
     select: (data) => data.find((v) => v.fk_product_child === variantId).attributes,
@@ -22,9 +20,9 @@ export const useFetchProduct = (variantId, defaultProductId, childCatId) => {
   if (defaultProductId == 0) {
     // is not a variant
     const isAllSuccess = noVariantSucceed && categorySucceed;
-console.log(isAllSuccess);
+    console.log(isAllSuccess);
 
-    return { allAttributes: null, defaultProduct: null, category: category, productAttributes: noVariantAttributes, isAllSuccess: isAllSuccess, allValues: null, isVariant: false };
+    return { allAttributes: null, defaultProduct: false, category: category, productAttributes: noVariantAttributes, isAllSuccess: isAllSuccess, allValues: null, isVariant: false };
   } else if (defaultProductId != undefined) {
     // is a variant
 
@@ -33,6 +31,5 @@ console.log(isAllSuccess);
     return { allAttributes, defaultProduct, category, productAttributes, isAllSuccess, allValues, isVariant: true };
   } else {
     return { allAttributes: null, defaultProduct: null, category: null, productAttributes: null, isAllSuccess: false, allValues: null, isVariant: null };
-
   }
 };
