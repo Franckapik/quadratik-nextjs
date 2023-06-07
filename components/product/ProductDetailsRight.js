15a5c6fcf38fcb8c504d3fb4ed6251701e95370b
Franckapik <fanch44@hotmail.com>
2023-06-07 17:14:47 +0200

    add cart btn and change quadralab link

--- components/product/ProductDetailsRight.js
+++ components/product/ProductDetailsRight.js
@@ -5,20 +5,17 @@ import { useRef } from "react";
 
 export const ProductDetailsRight = ({ product, display }) => {
   return (
-    <Row className="text_dark w-100 justify-content-center ">
-      {display != 3 ? (
-        <Row className="d-inline product_modele_desc">
-          <p>REF : {product.nomenclature.structurel}</p>
-          <p className="modele_phrase">{product.description.category_desc.replace("$PRODUCT", product.nomenclature.simple)}</p>
-        </Row>
-      ) : null}
-      <Row className="d-inline product_modele_desc_details justify-content-center">
+    <Col className="text_dark product_modele_desc d-flex flex-column justify-content-between ">
+      <Row className="d-inline ">
+        <p>REF : {product.nomenclature.structurel}</p>
+        <p className="modele_phrase">{product.description.category_desc.replace("$PRODUCT", product.nomenclature.simple)}</p>
+      </Row>
+      <Row className="d-inline justify-content-center">
         <Carousel activeIndex={display} controls={false} indicators={false}>
           <Carousel.Item>{product.description.parent_description}</Carousel.Item>
-
           <Carousel.Item>
-            <Row>
-              <Col>
+            <Row >
+              <Col className="d-flex flex-column justify-content-center align-items-center">
                 <PerformanceWidget icon="fad fa-bolt" value={`${product.dimensions.fmin} Hz - ${product.dimensions.fmax} Hz`} color="#f26565" performance={((product.dimensions.fmax - product.dimensions.fmin) * 100) / 10000} tooltip={"La plage de fréquence traitée"} />
                 <PerformanceWidget icon="fad fa-sort-size-down" value={`${(product.dimensions.c * 10).toFixed(0)} mm`} color="#f1b672" performance={100 - (product.dimensions.c * 10 * 100) / 90} tooltip={"La taille des cellules. Plus elle est petite, plus les aigus sont traités"} />
                 <PerformanceWidget
@@ -28,22 +25,20 @@ export const ProductDetailsRight = ({ product, display }) => {
                   performance={((product.dimensions.volume / 1000000) * 100) / 0.144}
                   tooltip={"L'aire traitée par le diffuseur et le volume (boite) qu'il occupe"}
                 />
-
               </Col>
             </Row>
           </Carousel.Item>
           <Carousel.Item>Spacialisation details</Carousel.Item>
         </Carousel>
       </Row>
-
-{/*       <Row className="product_right_cart">
-        <Col className="d-flex flex-column justify-content-center text-center align-items-center ft05">{product.prices.price + " €"}</Col>
-        <Col className="d-flex  justify-content-center text-center align-items-center">
+      <div className="product_right_cart">
+        <Row className="d-flex flex-column justify-content-center text-center align-items-center ft05">{product.prices.price + " €"}</Row>
+        <Row className="d-flex justify-content-center text-center align-items-center">
           <Button variant="primary" type="submit" id="product_submit" className="m-2">
             Ajouter au panier
           </Button>
-        </Col>
-      </Row> */}
-    </Row>
+        </Row>
+      </div>
+    </Col>
   );
 };
