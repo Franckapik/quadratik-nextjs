d0c92e6dced052df303ee3d208a0cfe0548e3083
Franckapik <fanch44@hotmail.com>
2023-06-08 15:23:24 +0200

    end of spinner when error on shop

--- components/shop/CardProduct.js
+++ components/shop/CardProduct.js
@@ -7,15 +7,14 @@ import { useComputeProduct } from "../../hooks/useComputeProduct";
 
 export const CardProduct = ({ variantId, defaultProductId, childCatId, childCatLabel }) => {
   const { allAttributes, defaultProduct, category, productAttributes, isAllSuccess, allValues, isVariant } = useFetchProduct(variantId, defaultProductId, childCatId);
-  const { product, isSuccess:  productSuccess } = useComputeProduct(allAttributes, productAttributes, allValues, category, defaultProduct, isAllSuccess, variantId, isVariant);
-  const { facePicture: facePicture, isSuccess: pictureSucceed } = usePicture(product.nomenclature?.simple, true); //true for miniature
-
+  const { product, isSuccess: productSuccess } = useComputeProduct(allAttributes, productAttributes, allValues, category, defaultProduct, isAllSuccess, variantId, isVariant);
+  const { facePicture: facePicture, isSuccess: pictureSucceed, isError: isPictureError } = usePicture(product.nomenclature?.simple, true); //true for miniature
   return (
     <>
       {productSuccess ? (
         <CardWrap>
           <Link href={{ pathname: "/shop/product", query: { display: 0, childCat: childCatId, vid: variantId, dpid: defaultProduct.id } }}>
-            {pictureSucceed ? <img src={`data:image/png;base64,${facePicture}`} /> : <i className="fas fa-spinner fa-spin"></i>}
+            {!isPictureError ? pictureSucceed ? <img src={`data:image/png;base64,${facePicture}`} /> : <i className="fas fa-spinner fa-spin"></i> : "Aperçu non disponible"}
             <span className="shop_product_title ft2 text-nowrap ">{product.nomenclature.simple}</span>
             <span className="shop_product_collection ft6 text-uppercase text-nowrap ">{childCatLabel.replace(/[0-9]{1}\s-\s*/gm, "")}</span>
             <span className="shop_product_price ft4  text-nowrap ">{product.prices ? product.prices.price + " €" : "Prix non disponible"} </span>
