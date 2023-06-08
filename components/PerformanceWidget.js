74ffece496799d4036fa8bff321c481f79a63848
Franckapik <fanch44@hotmail.com>
2023-06-08 10:07:18 +0200

    performance css responsive

--- components/PerformanceWidget.js
+++ components/PerformanceWidget.js
@@ -4,14 +4,14 @@ import { OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 export const PerformanceWidget = ({ icon, value, color, performance, tooltip }) => {
   return (
     <OverlayTrigger key={"left"} placement={"left"} overlay={<Tooltip id={`tooltip-${value}`}>{tooltip}</Tooltip>}>
-      <Row className="quadralab_performance_widget">
-        <div className="flex quadralab_round quadralab_game_border bg_dark ">
+      <Row className="performance_widget">
+        <div className="flex performance_round performance_border bg_dark ">
           <i className={icon}></i>
         </div>
-        <div className="flex quadralab_line quadralab_game_border ">
+        <div className="flex performance_line performance_border ">
           <div className="h-100" style={{ width: `${performance}%`, maxWidth: "100%", backgroundColor: color }}></div>
         </div>
-        <p className="quadralab_perf_value text-end">{value}</p>
+        <p className="performance_value text-end">{value}</p>
       </Row>
     </OverlayTrigger>
   );
