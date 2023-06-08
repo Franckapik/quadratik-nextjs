a2323f3cd4e1300c9fb136ca10c9f4c1ff8e48d7
Franckapik <fanch44@hotmail.com>
2023-06-08 16:20:50 +0200

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,15 @@ import Head from "next/head";
 //auto-generated ids are consistent between the server and client.
 import SSRProvider from "react-bootstrap/SSRProvider";
 import { QueryClient, QueryClientProvider } from "react-query";
-import {ReactQueryDevtools} from 'react-query/devtools';
-
+import { ReactQueryDevtools } from "react-query/devtools";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const queryClient = new QueryClient({
-  defaultOptions : {
-    queries : {
+  defaultOptions: {
+    queries: {
       refetchOnWindowFocus: false,
-    }
-  }
+    },
+  },
 });
 
 // This default export is required in a new `pages/_app.js` file.
@@ -25,8 +25,10 @@ export default function MyApp({ Component, pageProps }) {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
         </Head>
-{/*         <ReactQueryDevtools initialIsOpen={false} />
- */}        <Component {...pageProps} />
+        {/*         <ReactQueryDevtools initialIsOpen={false} />
+         */}
+        <GoogleAnalytics trackPageViews />
+        <Component {...pageProps} />
       </QueryClientProvider>
     </SSRProvider>
   );
