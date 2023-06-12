c3721d668f1363c8b750588354d34febd76db0bb
Franckapik <fanch44@hotmail.com>
2023-06-12 11:13:50 +0200

    add html lang fr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
-
 module.exports = {
+  i18n: {
+    locales: ['fr'],
+    defaultLocale: 'fr',
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
