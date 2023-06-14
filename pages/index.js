d538333eab080c98b39a9d2b29734c6c3e9ab7d6
Franckapik <fanch44@hotmail.com>
2023-06-14 19:57:11 +0200

    no showCanvas in Home

--- pages/index.js
+++ pages/index.js
@@ -7,50 +7,36 @@ import { useScroll } from "../hooks/useScroll";
 import dynamic from "next/dynamic";
 const S1_Product = dynamic(() => import("../components/home/S1_Product"), {
   suspense: true,
-  ssr:false
+  ssr: false,
 });
 const S0_Landing = dynamic(() => import("../components/home/S0_Landing"), {
   suspense: true,
-  ssr:false
-
+  ssr: false,
 });
 const S2_Canvas = dynamic(() => import("../components/home/S2_Canvas"), {
   suspense: true,
-  ssr:false
-
+  ssr: false,
 });
 const S2_Customers = dynamic(() => import("../components/home/S2_Customers"), {
   suspense: true,
-  ssr:false
-
+  ssr: false,
 });
 const S3_DIY = dynamic(() => import("../components/home/S3_DIY"), {
   suspense: true,
-  ssr:false
-
+  ssr: false,
 });
 const S4_Business = dynamic(() => import("../components/home/S4_Business"), {
   suspense: true,
-  ssr:false
-
+  ssr: false,
 });
 const S5_Contact = dynamic(() => import("../components/home/S5_Contact"), {
   suspense: true,
-  ssr:false
-
+  ssr: false,
 });
 
 const Home = () => {
   const parallax = useRef(null);
-  const [showCanvas, setShowCanvas] = useState(false);
   const [scroll, height, width] = useScroll(parallax);
-  useEffect(() => {
-    if (scroll > height * 3 && scroll < height * 5) {
-      setShowCanvas(true);
-    } else {
-      setShowCanvas(false);
-    }
-  }, [height, scroll]);
 
   return (
     <>
@@ -72,7 +58,10 @@ const Home = () => {
             <S2_Customers />
           </Suspense>
         </ParallaxLayer>
-        <ParallaxLayer offset={4}>{showCanvas ? <S2_Canvas /> : null}</ParallaxLayer>
+        <ParallaxLayer offset={4}>
+          {" "}
+          <S2_Canvas />{" "}
+        </ParallaxLayer>
         <ParallaxLayer offset={5}>
           <Suspense fallback={<div>...</div>}>
             <S3_DIY />
