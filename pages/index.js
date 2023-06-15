94cd2ac876f9aaa71f2d3c88f1c8e0d8dc98a61a
Franckapik <fanch44@hotmail.com>
2023-06-15 09:41:38 +0200

    back to rss  and canvas onScroll for better perf !

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import { Suspense, useEffect, useRef, useState } from "react";
 import { LayoutHome } from "../components/LayoutHome";
+
 import { VerticalSideIndex } from "../components/home/VerticalSideIndex";
 import { useScroll } from "../hooks/useScroll";
 import dynamic from "next/dynamic";
@@ -35,7 +36,15 @@ const S5_Contact = dynamic(() => import("../components/home/S5_Contact"), {
 
 const Home = () => {
   const parallax = useRef(null);
+  const [showCanvas, setShowCanvas] = useState(false);
   const [scroll, height, width] = useScroll(parallax);
+  useEffect(() => {
+    if (scroll > height * 3 && scroll < height * 5) {
+      setShowCanvas(true);
+    } else {
+      setShowCanvas(false);
+    }
+  }, [height, scroll]);
 
   return (
     <>
@@ -57,9 +66,7 @@ const Home = () => {
             <S2_Customers />
           </Suspense>
         </ParallaxLayer>
-        <ParallaxLayer offset={4}>
-coucou
-        </ParallaxLayer>
+        <ParallaxLayer offset={4}>{showCanvas && <S2_Canvas />}</ParallaxLayer>
         <ParallaxLayer offset={5}>
           <Suspense fallback={<div>...</div>}>
             <S3_DIY />
